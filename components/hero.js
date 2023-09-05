9e4edc900e299e3d3588ce1e65cd767225b6f5c4
Jesper Kjeldgaard <thejspr@gmail.com>
2023-09-05 11:52:01 +0200

    reduce title size

--- components/hero.js
+++ components/hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
-            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+            <h1 className="text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
               Hurtig og Pålidelig Software Udvikling
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
